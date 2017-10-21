a310984da633c04b6242d6c7953e0381cccc71b4
hunter-s70 <serg.buhtoyarov@gmail.com>
2017-10-21 15:47:58 +0400

    add step components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Step1Component } from './step1/step1.component';
+import { Step2Component } from './step2/step2.component';
+import { Step3Component } from './step3/step3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Step1Component,
+    Step2Component,
+    Step3Component
   ],
   imports: [
     BrowserModule
