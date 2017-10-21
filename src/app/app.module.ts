9968182165984f7e08c264074b9992d06477a4ad
hunter-s70 <serg.buhtoyarov@gmail.com>
2017-10-21 17:40:34 +0400

    add routing steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {Routes, RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { Step1Component } from './step1/step1.component';
 import { Step2Component } from './step2/step2.component';
 import { Step3Component } from './step3/step3.component';
+
+export const appRoutes: Routes = [
+  { path: '', component: Step1Component},
+  { path: 'st2', component: Step2Component, pathMatch: 'full' },
+  { path: 'st3', component: Step3Component, pathMatch: 'full' },
+  { path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +23,8 @@ import { Step3Component } from './step3/step3.component';
     Step3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
